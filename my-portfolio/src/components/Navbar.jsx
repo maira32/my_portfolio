87e1983c4f31e250ff4e895303dcceb7252f48d9
maira32 <mairatahir3@gmail.com>
2026-06-16 00:57:34 +0500

    Forced lowercase logo file to fix Vercel Linux build

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Link } from 'react-router-dom';
-import logoImage from '../assets/my-logo.png'; // Importing your new logo
+import logoImage from '../assets/logo.png'; // Importing your new logo
 
 const Navbar = () => {
   return (
